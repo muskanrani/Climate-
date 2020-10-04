8616d67022edd3e30ecec20d6766684594e64482
muskanrani <muskanrani@users.noreply.github.com>
2020-10-04 14:38:50 +0530

    Added spinner in loading screen and fetching weather data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,22 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
+const apiKey = '3aec92dea777a013fd9ee9c488d58e2e';
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude,longitude;
+  @override
+  void initState() {
+    super.initState();
+    geoLocationData();
+  }
+
+  void geoLocationData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    latitude = location.latitude;
+    longitude = location.longitude;
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    var weatherData = await networkHelper.getData();
+    Navigator.push(context, MaterialPageRoute(builder: (context){
+      return LocationScreen();
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
         ),
       ),
     );
   }
 }
+
